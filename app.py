d5f2875b7e079dcc868239a56e23d9902ea78625
MathCatsAnd <98661771+MathCatsAnd@users.noreply.github.com>
2023-01-18 20:37:44 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ import os
 st.write('Test App')
 output = os.popen('pip list').read()
 st.markdown(output.replace('\n','  \n'))
+
+try:
+  import matplotlib
+  st.write('environment.yml correctly installed matplotlib')
+catch:
+  st.write('environment not activated')
+  out = os.popen('''conda activate myenv
+pip list''')
